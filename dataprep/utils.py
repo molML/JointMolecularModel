ce896c8cc1c0618df012cfbfd9fbe00fed876067
derekvantilborg <dwvantilborg@gmail.com>
2024-02-28 10:23:14 +0100

    added some utils

--- dataprep/utils.py
+++ dataprep/utils.py
@@ -13,11 +13,14 @@ Eindhoven University of Technology
 Jan 2024
 """
 
+from typing import Union
+import numpy as np
+from tqdm.auto import tqdm
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.rdchem import Mol
 from rdkit.Chem.Scaffolds.MurckoScaffold import GetScaffoldForMol, MakeScaffoldGeneric
-from typing import Union
+from rdkit.DataStructs import BulkTanimotoSimilarity
 
 
 def canonicalize_smiles(smiles: Union[str, list[str]]) -> Union[str, list[str]]:
@@ -75,3 +78,29 @@ def mols_to_scaffolds(mols: list[Mol], scaffold_type: str = 'bismurcko') -> list
         scaffolds = [GetScaffoldForMol(m) for m in mols]
 
     return scaffolds
+
+
+def tanimoto_matrix(fingerprints: list, progressbar: bool = False, fill_diagonal: bool = True, dtype=np.float16) \
+        -> np.ndarray:
+    """
+
+    :param fingerprints: list of RDKit fingerprints
+    :param progressbar: toggles progressbar (default = False)
+    :param dtype: numpy dtype (default = np.float16)
+    :param fill_diagonal: Fill the diagonal with 1's (default = True)
+
+    :return: Tanimoto similarity matrix
+    """
+    n = len(fingerprints)
+
+    X = np.zeros([n, n], dtype=dtype)
+    # Fill the upper triangle of the pairwise matrix
+    for i in tqdm(range(n), disable=not progressbar, desc=f"Computing pairwise Tanimoto similarity of {n} molecules"):
+        X[i, i+1:] = BulkTanimotoSimilarity(fingerprints[i], fingerprints[i+1:])
+    # Mirror out the lower triangle
+    X = X + X.T - np.diag(np.diag(X))
+
+    if fill_diagonal:
+        np.fill_diagonal(X, 1)
+
+    return X
