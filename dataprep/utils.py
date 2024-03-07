4b50df288f1cba9d7ca596583482bf2e83a6d08a
derekvantilborg <dwvantilborg@gmail.com>
2024-03-07 15:28:51 +0100

    Added SMILES tokenizer and scaffold mapping

--- dataprep/utils.py
+++ dataprep/utils.py
@@ -13,7 +13,9 @@ Eindhoven University of Technology
 Jan 2024
 """
 
+import re
 from typing import Union
+from collections import defaultdict
 import numpy as np
 from tqdm.auto import tqdm
 from rdkit import Chem
@@ -40,6 +42,11 @@ def smiles_to_mols(smiles: list[str], sanitize: bool = True, partial_charges: bo
     :return: List of RDKit mol objects
     """
     mols = []
+    was_list = True
+    if type(smiles) is str:
+        was_list = False
+        smiles = [smiles]
+
     for smi in smiles:
         molecule = Chem.MolFromSmiles(smi, sanitize=sanitize)
 
@@ -57,12 +64,12 @@ def smiles_to_mols(smiles: list[str], sanitize: bool = True, partial_charges: bo
 
         mols.append(molecule)
 
-    return mols
+    return mols if was_list else mols[0]
 
 
 def mols_to_smiles(mols: list[Mol]) -> list[str]:
     """ Convert a list of RDKit molecules objects into a list of SMILES strings"""
-    return [Chem.MolToSmiles(m) for m in mols]
+    return [Chem.MolToSmiles(m) for m in mols] if type(mols) is list else Chem.MolToSmiles(mols)
 
 
 def mols_to_scaffolds(mols: list[Mol], scaffold_type: str = 'bismurcko') -> list:
@@ -104,3 +111,45 @@ def tanimoto_matrix(fingerprints: list, progressbar: bool = False, fill_diagonal
         np.fill_diagonal(X, 1)
 
     return X
+
+
+def smiles_tokenizer(smiles: str, extra_patterns: list[str] = None) -> list[str]:
+    """ Tokenize a SMILES. By default, we use the base SMILES grammar tokens and the reactive nonmetals H, C, N, O, F,
+    P, S, Cl, Se, Br, I:
+
+    '(\\[|\\]|Cl|Se|se|Br|H|C|c|N|n|O|o|F|P|p|S|s|I|\\(|\\)|\\.|=|#|-|\\+|\\\\|\\/|:|~|@|\\?|>|\\*|\\$|\\%\\d{2}|\\d)'
+
+    :param smiles: SMILES string
+    :param extra_patterns: extra tokens to consider (default = None)
+        e.g. metalloids: ['Si', 'As', 'Te', 'te', 'B', 'b']  (in ChEMBL33: B+b=0.23%, Si=0.13%, As=0.01%, Te+te=0.01%).
+        Mind you that the order matters. If you place 'C' before 'Cl', all Cl tokens will actually be tokenized as C,
+        meaning that subsets should always come after superset strings, aka, place two letter elements first in the list
+    :return: list of tokens extracted from the smiles string in their original order
+    """
+    base_smiles_patterns = "(\[|\]|insert_here|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%\d{2}|\d)"
+    reactive_nonmetals = ['Cl', 'Si', 'si', 'Se', 'se', 'Br', 'B', 'H', 'C', 'c', 'N', 'n', 'O', 'o', 'F', 'P', 'p', 'S', 's', 'I']
+
+    # Add all allowed elements to the base SMILES tokens
+    extra_patterns = reactive_nonmetals if extra_patterns is None else extra_patterns + reactive_nonmetals
+    pattern = base_smiles_patterns.replace('insert_here', "|".join(extra_patterns))
+
+    regex = re.compile(pattern)
+    tokens = [token for token in regex.findall(smiles)]
+
+    return tokens
+
+
+def map_scaffolds(mols: list) -> (list, dict[str, list[int]]):
+    """ Find which molecules share the same scaffold
+
+    :param mols: RDKit mol objects, e.g., as obtained through smiles_to_mols()
+    :return: scaffolds, dict of unique scaffolds and which molecules (indices) share them -> {'c1ccccc1': [0, 12, 47]}
+    """
+
+    scaffolds = mols_to_scaffolds(mols)
+
+    uniques = defaultdict(list)
+    for i, s in enumerate(scaffolds):
+        uniques[Chem.MolToSmiles(s)].append(i)
+
+    return scaffolds, uniques
