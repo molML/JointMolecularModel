0cbbea208a7db844a007c988c26c1dc93f726aea
derekvantilborg <dwvantilborg@gmail.com>
2024-03-18 11:14:28 +0100

    changed latent dim and train duration

--- experiments/pretrain_vae.py
+++ experiments/pretrain_vae.py
@@ -6,9 +6,13 @@ from jcm.trainer import train_vae
 from jcm.config import Config
 from constants import VAE_PRETRAIN_HYPERPARAMETERS
 from constants import ROOTDIR
-
 OUT_DIR = os.path.join(ROOTDIR, 'results/chembl_vae')
 
+
+#### Hyperparameters space
+# Beta: 1e-2, 1e-3, 1e-4
+# lr:   3e-3, 3e-4, 3e-5
+# variational_scale: 0.1, 0.01, 0.001
 
 if __name__ == '__main__':
 
@@ -18,7 +22,9 @@ if __name__ == '__main__':
     val_smiles = pd.read_csv('data/ChEMBL/val_smiles.csv').smiles.tolist()
     val_dataset = MoleculeDataset(val_smiles, descriptor='ecfp')
 
-    config = Config(max_iters=10000, batch_size=128, batch_end_callback_every=150, out_path=OUT_DIR)
+    VAE_PRETRAIN_HYPERPARAMETERS['latent_dim'] = 64
+
+    config = Config(max_iters=200000, batch_size=256, batch_end_callback_every=1000, out_path=OUT_DIR)
     config.set_hyperparameters(**VAE_PRETRAIN_HYPERPARAMETERS)
 
-    model = train_vae(config, train_dataset, val_dataset)
+    model, trainer = train_vae(config, train_dataset, val_dataset)
