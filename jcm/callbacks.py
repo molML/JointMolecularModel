257cd48f91269a741aa0e473a38636b77cfc3c35
derekvantilborg <dwvantilborg@gmail.com>
2024-03-18 11:12:07 +0100

    Added torch nograd decorators

--- jcm/callbacks.py
+++ jcm/callbacks.py
@@ -10,133 +10,136 @@ from torch.utils.data import RandomSampler
 from jcm.utils import to_binary, ClassificationMetrics, logits_to_pred, predict_and_eval_mlp, single_batchitem_fix
 
 
+@torch.no_grad()
 def vae_batch_end_callback(trainer):
 
     config = trainer.config
+    if config.batch_end_callback_every is not None:
+        if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
+            metrics_list = []
+            losses = []
 
-    if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
-        metrics_list = []
-        losses = []
+            if config.out_path is not None:
+                ckpt_path = os.path.join(config.out_path, f"pretrained_vae_{trainer.iter_num}.pt")
+                torch.save(trainer.model.state_dict(), ckpt_path)
 
-        if config.out_path is not None:
-            ckpt_path = os.path.join(config.out_path, f"pretrained_vae_{trainer.iter_num}.pt")
-            torch.save(trainer.model.state_dict(), ckpt_path)
+            val_loader = DataLoader(trainer.val_dataset,
+                                    sampler=RandomSampler(trainer.val_dataset, replacement=True,
+                                                          num_samples=trainer.config.val_molecules_to_sample),
+                                    shuffle=False, pin_memory=True, batch_size=trainer.config.batch_size,
+                                    collate_fn=single_batchitem_fix)
 
-        val_loader = DataLoader(trainer.val_dataset,
-                                sampler=RandomSampler(trainer.val_dataset, replacement=True,
-                                                      num_samples=trainer.config.val_molecules_to_sample),
-                                shuffle=False, pin_memory=True, batch_size=trainer.config.batch_size,
-                                collate_fn=single_batchitem_fix)
+            trainer.model.eval()
+            for batch in tqdm(val_loader):
+                # break
+                batch.to(config.device)
+                x = batch
 
-        trainer.model.eval()
-        for batch in tqdm(val_loader):
-            # break
-            batch.to(config.device)
-            x = batch
+                x_hat, z, sample_likelihood, loss = trainer.model(x)
+                losses.append(loss.item())
 
-            x_hat, z, sample_likelihood, loss = trainer.model(x)
-            losses.append(loss.item())
+                x_hat_bin = to_binary(x_hat)
+                metrics_list.extend([ClassificationMetrics(x[i], x_hat_bin[i]).__dict__ for i in range(len(x))])
 
-            x_hat_bin = to_binary(x_hat)
-            metrics_list.extend([ClassificationMetrics(x[i], x_hat_bin[i]).__dict__ for i in range(len(x))])
+            trainer.model.train()
 
-        trainer.model.train()
+            mean_val_loss = sum(losses) / len(losses)
+            mean_metrics = {key: np.mean([d[key] for d in metrics_list]) for key in metrics_list[0]}
 
-        mean_val_loss = sum(losses) / len(losses)
-        mean_metrics = {key: np.mean([d[key] for d in metrics_list]) for key in metrics_list[0]}
+            trainer.history['iter_num'].append(trainer.iter_num)
+            trainer.history['train_loss'].append(trainer.loss.item())
+            trainer.history['val_loss'].append(mean_val_loss)
 
-        trainer.history['iter_num'].append(trainer.iter_num)
-        trainer.history['train_loss'].append(trainer.loss.item())
-        trainer.history['val_loss'].append(mean_val_loss)
+            for k, v in mean_metrics.items():
+                trainer.history[f"val_{k}"].append(v)
 
-        for k, v in mean_metrics.items():
-            trainer.history[f"val_{k}"].append(v)
+            print(f"Iter: {trainer.iter_num}, train loss: {round(trainer.loss.item(), 4)}, "
+                  f"val loss: {round(mean_val_loss, 4)}, balanced accuracy: {round(mean_metrics['BA'], 4)}, "
+                  f"100% reconstruction: {round(mean_metrics['recons_100'], 4)}, "
+                  f"99% reconstruction {round(mean_metrics['recons_99'], 4)}, recall: {round(mean_metrics['TPR'], 4)},"
+                  f" precision: {round(mean_metrics['PPV'], 4)}")
 
-        print(f"Iter: {trainer.iter_num}, train loss: {round(trainer.loss.item(), 4)}, "
-              f"val loss: {round(mean_val_loss, 4)}, balanced accuracy: {round(mean_metrics['BA'], 4)}, "
-              f"100% reconstruction: {round(mean_metrics['recons_100'], 4)}, "
-              f"99% reconstruction {round(mean_metrics['recons_99'], 4)}, recall: {round(mean_metrics['TPR'], 4)},"
-              f" precision: {round(mean_metrics['PPV'], 4)}")
-
-        if trainer.config.out_path is not None:
-            history_path = os.path.join(config.out_path, f"training_history.csv")
-            trainer.get_history(history_path)
+            if trainer.config.out_path is not None:
+                history_path = os.path.join(config.out_path, f"training_history.csv")
+                trainer.get_history(history_path)
 
 
+@torch.no_grad()
 def mlp_batch_end_callback(trainer):
 
     config = trainer.config
+    if config.batch_end_callback_every is not None:
+        if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
+            y_hats = []
+            y_trues = []
+            losses = []
 
-    if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
-        y_hats = []
-        y_trues = []
-        losses = []
+            if config.out_path is not None:
+                ckpt_path = os.path.join(config.out_path, f"mlp_{trainer.iter_num}.pt")
+                torch.save(trainer.model.state_dict(), ckpt_path)
 
-        if config.out_path is not None:
-            ckpt_path = os.path.join(config.out_path, f"mlp_{trainer.iter_num}.pt")
-            torch.save(trainer.model.state_dict(), ckpt_path)
+            val_loader = DataLoader(trainer.val_dataset, shuffle=False, pin_memory=True, batch_size=config.batch_size,
+                                    collate_fn=single_batchitem_fix)
 
-        val_loader = DataLoader(trainer.val_dataset, shuffle=False, pin_memory=True, batch_size=config.batch_size,
-                                collate_fn=single_batchitem_fix)
+            trainer.model.eval()
+            for x, y in val_loader:
+                x.to(config.device)
+                y.to(config.device)
 
-        trainer.model.eval()
-        for x, y in val_loader:
-            x.to(config.device)
-            y.to(config.device)
+                y_hat, loss = trainer.model(x, y)
 
-            y_hat, loss = trainer.model(x, y)
+                losses.append(loss.item())
+                y_hats.append(y_hat)
+                y_trues.append(y)
 
-            losses.append(loss.item())
-            y_hats.append(y_hat)
-            y_trues.append(y)
+            trainer.model.train()
 
-        trainer.model.train()
+            # merge the predictions over batches
+            mean_val_loss = sum(losses) / len(losses)
+            y_hats = torch.cat(y_hats, 0)
+            y_trues = torch.cat(y_trues, 0)
 
-        # merge the predictions over batches
-        mean_val_loss = sum(losses) / len(losses)
-        y_hats = torch.cat(y_hats, 0)
-        y_trues = torch.cat(y_trues, 0)
+            # compute balanced accuracy
+            preds, uncertainty = logits_to_pred(y_hats, return_binary=True, return_uncertainty=True)
+            ba = ClassificationMetrics(y=y_trues, y_hat=preds).BA
 
-        # compute balanced accuracy
-        preds, uncertainty = logits_to_pred(y_hats, return_binary=True, return_uncertainty=True)
-        ba = ClassificationMetrics(y=y_trues, y_hat=preds).BA
+            # write to file
+            trainer.history['iter_num'].append(trainer.iter_num)
+            trainer.history['train_loss'].append(trainer.loss.item())
+            trainer.history['val_loss'].append(mean_val_loss)
+            trainer.history['val_ba'].append(ba)
 
-        # write to file
-        trainer.history['iter_num'].append(trainer.iter_num)
-        trainer.history['train_loss'].append(trainer.loss.item())
-        trainer.history['val_loss'].append(mean_val_loss)
-        trainer.history['val_ba'].append(ba)
+            print(f"Iter: {trainer.iter_num}, train loss: {round(trainer.loss.item(), 4)}, "
+                  f"val loss: {round(mean_val_loss, 4)}, balanced accuracy: {round(ba, 4)}")
 
-        print(f"Iter: {trainer.iter_num}, train loss: {round(trainer.loss.item(), 4)}, "
-              f"val loss: {round(mean_val_loss, 4)}, balanced accuracy: {round(ba, 4)}")
-
-        if config.out_path is not None:
-            history_path = os.path.join(config.out_path, f"training_history.csv")
-            trainer.get_history(history_path)
+            if config.out_path is not None:
+                history_path = os.path.join(config.out_path, f"training_history.csv")
+                trainer.get_history(history_path)
 
 
+@torch.no_grad()
 def jvae_batch_end_callback(trainer):
 
     config = trainer.config
+    if config.batch_end_callback_every is not None:
+        if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
 
-    if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
+            losses = []
+            val_loader = DataLoader(trainer.val_dataset,
+                                    sampler=RandomSampler(trainer.val_dataset, replacement=True,
+                                                          num_samples=trainer.config.val_molecules_to_sample),
+                                    shuffle=False, pin_memory=True, batch_size=trainer.config.batch_size,
+                                    collate_fn=single_batchitem_fix)
 
-        losses = []
-        val_loader = DataLoader(trainer.val_dataset,
-                                sampler=RandomSampler(trainer.val_dataset, replacement=True,
-                                                      num_samples=trainer.config.val_molecules_to_sample),
-                                shuffle=False, pin_memory=True, batch_size=trainer.config.batch_size,
-                                collate_fn=single_batchitem_fix)
+            trainer.model.eval()
+            for x, y in val_loader:
 
-        trainer.model.eval()
-        for x, y in val_loader:
+                x.to(trainer.config.device)
+                y.to(trainer.config.device)
 
-            x.to(trainer.config.device)
-            y.to(trainer.config.device)
+                y_hat, z, sample_likelihood, loss = trainer.model(x, y)
+                losses.append(loss.item())
 
-            y_hat, z, sample_likelihood, loss = trainer.model(x, y)
-            losses.append(loss.item())
-
-        trainer.model.train()
-        mean_val_loss = sum(losses) / len(losses)
-        print(trainer.iter_num, trainer.loss.item(), mean_val_loss)
+            trainer.model.train()
+            mean_val_loss = sum(losses) / len(losses)
+            print(trainer.iter_num, trainer.loss.item(), mean_val_loss)
