00f1861b90f8449a944065eb83cc2ef146a2a2ce
derekvantilborg <dwvantilborg@gmail.com>
2024-03-18 16:45:25 +0100

    fixed lr disappearing from config bug

--- jcm/config.py
+++ jcm/config.py
@@ -14,11 +14,12 @@ class Config:
         self.merge_from_dict(kwargs)
 
     def set_hyperparameters(self, **kwargs):
-        self.hyperparameters = kwargs
-        self.merge_from_dict(kwargs)
+        self.hyperparameters.update(kwargs)
+        self.merge_from_dict(self.hyperparameters)
 
     def merge_from_dict(self, d):
         self.__dict__.update(d)
 
     def __repr__(self):
-        return str(self.__dict__).replace(', ', '\n').replace('{', '').replace('}', '')
+        vals = {k: v for k, v in self.__dict__.items() if k != 'hyperparameters'}
+        return str(vals).replace(', ', '\n').replace('{', '').replace('}', '')
