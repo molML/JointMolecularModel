632e3b7a5e613b22e57a65804ec10a33e36435de
derekvantilborg <dwvantilborg@gmail.com>
2024-03-18 11:12:36 +0100

    revamped MLP pretraining

--- experiments/pretrain_mlp.py
+++ experiments/pretrain_mlp.py
@@ -15,88 +15,111 @@ from constants import ROOTDIR
 OUT_DIR = os.path.join(ROOTDIR, 'results/pre_trained_mlps')
 OUT_FILE_HYPERTUNING = 'mlp_hyper_tuning.csv'
 OUT_FILE_RESULTS = 'mlp_pretraining_results.csv'
-N_FOLDS = 5
+N_FOLDS = 10
 VAL_SPLIT = 0.2
 MAX_ITERS = 100
 BATCH_SIZE = 128
 
-DEFAULT_HYPERPARAMETERS = {"input_dim": 1024, "hidden_dim": 1024, "output_dim": 2, "anchored": True, "l2_lambda": 3e-4,
-                           "n_ensemble": 10}
+DEFAULT_HYPERPARAMETERS = {"input_dim": 2048, "hidden_dim": 2048, "output_dim": 2, "anchored": True, "l2_lambda": 3e-4,
+                           "n_ensemble": 10, 'latent_dim': 64}
 
 n_layers = [1, 2, 3]
 lr = [3e-3, 3e-4, 3e-5]
 TUNEABLE_HYPERPARAMETERS = [{'n_layers': i[0], 'lr': i[1]} for i in itertools.product(n_layers, lr)]
 
 
+def hyper_param_tuning(dataset, hyperparameters: list[dict], folds: int = 10, val_split: float = 0.2,
+                       max_iters: int = 100, batch_size: int = 100):
+    scores = []
+    for hypers in tqdm(hyperparameters):
+        seed_score = []
+        for seed in range(folds):
+            try:
+                # get the data splits
+                train_dataset, val_dataset, test_dataset = load_moleculeace(dataset, val_split=val_split, seed=seed)
+
+                # add the hypers of this iteration to the default hypers
+                _hyperparameters = DEFAULT_HYPERPARAMETERS | hypers
+
+                # init config
+                config = Config(max_iters=max_iters, batch_size=batch_size, batch_end_callback_every=None, out_path=None)
+                config.set_hyperparameters(**_hyperparameters)
+
+                # train model
+                model, trainer = train_mlp(config, train_dataset, val_dataset)
+
+                # calculate metrics
+                metrics = predict_and_eval_mlp(model, val_dataset)
+                seed_score.append(metrics['BA'])
+
+            except:
+                print(f"Failed {dataset} {hypers} {seed}")
+
+        scores.append(sum(seed_score) / len(seed_score))
+
+    return hyperparameters[scores.index(max(scores))]
+
+
+def cross_validate(dataset, best_hypers, folds: int = 10, val_split: float = 0.2, save_path: str = None,
+                   max_iters: int = 100, batch_size: int = 100):
+
+    dataset_name = dataset.split('/')[-1].split('.csv')[0]
+    results = defaultdict(list)
+
+    # add the hypers of this iteration to the default hypers
+    _hyperparameters = DEFAULT_HYPERPARAMETERS | best_hypers
+
+    # init config
+    config = Config(max_iters=max_iters, batch_size=batch_size, batch_end_callback_every=None, out_path=None)
+    config.set_hyperparameters(**_hyperparameters)
+
+    for seed in range(folds):
+
+        train_dataset, val_dataset, test_dataset = load_moleculeace(dataset, val_split=val_split, seed=seed)
+
+        # train model
+        model, trainer = train_mlp(config, train_dataset, val_dataset)
+
+        val_metrics = predict_and_eval_mlp(model, val_dataset)
+        val_metrics = {'val_' + k: v for k, v in val_metrics.items()}
+
+        test_metrics = predict_and_eval_mlp(model, test_dataset)
+        test_metrics = {'test_' + k: v for k, v in test_metrics.items()}
+
+        results_fold = {'dataset': dataset_name, 'fold': seed} | val_metrics | test_metrics
+        for k, v in results_fold.items():
+            results[k].append(v)
+
+    # train final model
+    train_dataset, val_dataset, test_dataset = load_moleculeace(dataset, val_split=0)
+
+    # train model
+    model, trainer = train_mlp(config, train_dataset, val_dataset)
+
+    if save_path is not None:
+        pd.DataFrame(results).to_csv(os.path.join(save_path, f"{dataset_name}_mlp_results.csv"))
+        torch.save(model, os.path.join(save_path, f"{dataset_name}_mlp.pt"))
+
+    return model
+
+
 if __name__ == '__main__':
 
     datasets = [f'data/moleculeace/{i}' for i in os.listdir('data/moleculeace') if i.startswith('CHEMBL')]
-    history = defaultdict(list)
+    hyper_dict = {}
 
-    for dataset in tqdm(datasets):
+    for dataset in datasets:
+        dataset_name = dataset.split('/')[-1].split('.csv')[0]
 
-        for hypers in TUNEABLE_HYPERPARAMETERS:
-            for seed in range(N_FOLDS):
-                try:
-                    # get the data splits
-                    train_dataset, val_dataset, test_dataset = load_moleculeace(dataset, val_split=VAL_SPLIT, seed=seed)
+        best_hypers = hyper_param_tuning(dataset, TUNEABLE_HYPERPARAMETERS, folds=N_FOLDS, max_iters=MAX_ITERS,
+                                         batch_size=BATCH_SIZE, val_split=VAL_SPLIT)
 
-                    # add the hypers of this iteration to the default hypers
-                    hyperparameters = DEFAULT_HYPERPARAMETERS | hypers
+        hyper_dict[dataset_name] = best_hypers
+        torch.save(hyper_dict, 'results/best_hyperparameters.pt')
 
-                    # init config
-                    config = Config(max_iters=MAX_ITERS, batch_size=BATCH_SIZE, batch_end_callback_every=MAX_ITERS, out_path=None)
-                    config.set_hyperparameters(**hyperparameters)
+        model = cross_validate(dataset, best_hypers, folds=N_FOLDS, max_iters=MAX_ITERS, batch_size=BATCH_SIZE,
+                               val_split=VAL_SPLIT, save_path='results/pre_trained_mlps')
 
-                    # train model
-                    model, trainer = train_mlp(config, train_dataset, val_dataset)
 
-                    # calculate metrics
-                    metrics = predict_and_eval_mlp(model, val_dataset)
 
-                    history['dataset'].append(dataset.split('/')[-1].split('.csv')[0])
-                    for hyper_name, val in hypers.items():
-                        history[hyper_name].append(val)
-                    history['seed'].append(seed)
-                    history['BA'].append(metrics['BA'])
 
-                    df = pd.DataFrame(history)
-                    df.to_csv(os.path.join(OUT_DIR, OUT_FILE_HYPERTUNING))
-                except:
-                    print(f"Failed {dataset} {hypers} {seed}")
-
-    # find the best hypers for each dataset using some pandas magic
-    hyper_results = pd.read_csv(os.path.join(OUT_DIR, OUT_FILE_HYPERTUNING), index_col=False).iloc[:, 1:]
-    hyper_results = hyper_results.groupby(['dataset', 'n_layers', 'lr']).agg({'BA': ['mean', 'std']}).reset_index()
-    hyper_results.columns = [''.join(col).strip() for col in hyper_results.columns.values]
-    best_hypers = hyper_results.loc[hyper_results.groupby('dataset')['BAmean'].idxmax()]
-
-    # Train all models using the best hypers using all training data. Evaluate on test set
-    results = []
-    for dataset_name in tqdm(best_hypers.dataset):
-
-        n_layers = best_hypers[best_hypers['dataset'] == dataset_name].n_layers.item()
-        lr = best_hypers[best_hypers['dataset'] == dataset_name].lr.item()
-
-        # get the data splits
-        train_dataset, val_dataset, test_dataset = load_moleculeace(f"data/moleculeace/{dataset_name}.csv", val_split=0)
-
-        # add the hypers of this iteration to the default hypers
-        hyperparameters = DEFAULT_HYPERPARAMETERS | {'n_layers': n_layers, 'lr': lr}
-
-        # init config
-        config = Config(max_iters=MAX_ITERS, batch_size=BATCH_SIZE, out_path=None)
-        config.set_hyperparameters(**hyperparameters)
-
-        # train model
-        model, trainer = train_mlp(config, train_dataset)
-
-        # save model
-        torch.save(model, os.path.join(OUT_DIR, f"MLP_{dataset_name}.pt"))
-
-        # calculate metrics
-        metrics = predict_and_eval_mlp(model, test_dataset)
-        results.append(metrics)
-
-    # write file with all results
-    pd.concat([best_hypers.reset_index(), pd.DataFrame(results)], axis=1).to_csv(os.path.join(OUT_DIR, OUT_FILE_RESULTS))
