21931cbff3d322090d899d098985028d1bf58b83
derekvantilborg <dwvantilborg@gmail.com>
2024-03-20 14:47:17 +0100

    Added callback from JVAE and cleaned up the inference loop

--- jcm/callbacks.py
+++ jcm/callbacks.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data import RandomSampler
-from jcm.utils import to_binary, ClassificationMetrics, logits_to_pred, predict_and_eval_mlp, single_batchitem_fix
+from jcm.utils import to_binary, ClassificationMetrics, logits_to_pred, single_batchitem_fix, reconstruction_metrics
 
 
 @torch.no_grad()
@@ -29,22 +29,23 @@ def vae_batch_end_callback(trainer):
                                     shuffle=False, pin_memory=True, batch_size=trainer.config.batch_size,
                                     collate_fn=single_batchitem_fix)
 
+            xs, xhats = [], []
+
             trainer.model.eval()
             for batch in tqdm(val_loader):
-                # break
                 batch.to(config.device)
                 x = batch
 
                 x_hat, z, sample_likelihood, loss = trainer.model(x)
-                losses.append(loss.item())
 
-                x_hat_bin = to_binary(x_hat)
-                metrics_list.extend([ClassificationMetrics(x[i], x_hat_bin[i]).__dict__ for i in range(len(x))])
+                losses.append(loss.item())
+                xs.append(xs)
+                xhats.append(xhats)
 
             trainer.model.train()
 
             mean_val_loss = sum(losses) / len(losses)
-            mean_metrics = {key: np.mean([d[key] for d in metrics_list]) for key in metrics_list[0]}
+            mean_metrics = reconstruction_metrics(torch.cat(xhats), torch.cat(xs))
 
             trainer.history['iter_num'].append(trainer.iter_num)
             trainer.history['train_loss'].append(trainer.loss.item())
@@ -125,6 +126,8 @@ def jvae_batch_end_callback(trainer):
         if trainer.iter_num % config.batch_end_callback_every == 0 and trainer.iter_num > 0:
 
             losses = []
+            xs, x_hats = [], []
+            ys, y_hats = [], []
             val_loader = DataLoader(trainer.val_dataset,
                                     sampler=RandomSampler(trainer.val_dataset, replacement=True,
                                                           num_samples=trainer.config.val_molecules_to_sample),
@@ -137,9 +140,25 @@ def jvae_batch_end_callback(trainer):
                 x.to(trainer.config.device)
                 y.to(trainer.config.device)
 
-                y_hat, z, sample_likelihood, loss = trainer.model(x, y)
+                y_logits_N_K_C, x_hat, z, sample_likelihood, loss = trainer.model(x, y)
+
                 losses.append(loss.item())
+                xs.append(x)
+                x_hats.append(x_hat)
+                ys.append(y)
+                y_hats.append(y_logits_N_K_C)
 
             trainer.model.train()
+
             mean_val_loss = sum(losses) / len(losses)
-            print(trainer.iter_num, trainer.loss.item(), mean_val_loss)
+
+            preds, uncertainty = logits_to_pred(torch.cat(y_hats), return_binary=True, return_uncertainty=True)
+            ba = ClassificationMetrics(y=torch.cat(ys), y_hat=preds).BA
+
+            mean_metrics = reconstruction_metrics(torch.cat(x_hats), torch.cat(xs))
+
+            print(f"Iter: {trainer.iter_num}, train loss: {round(trainer.loss.item(), 4)}, "
+                  f"val loss: {round(mean_val_loss, 4)}, balanced accuracy y: {round(ba, 4)}, balanced accuracy: "
+                  f"{round(mean_metrics['BA'], 4)}, 100% reconstruction: {round(mean_metrics['recons_100'], 4)}, "
+                  f"99% reconstruction {round(mean_metrics['recons_99'], 4)}, recall: {round(mean_metrics['TPR'], 4)},"
+                  f" precision: {round(mean_metrics['PPV'], 4)}")
