fa7797adeb21bbb2732b3b74f8b2c5b646bb30fc
derekvantilborg <dwvantilborg@gmail.com>
2024-04-11 11:38:00 +0200

    added a function for uniqueness, novelty, reconstruction, tanimoto, edit distance, and plotting

--- eval/design_eval.py
+++ eval/design_eval.py
@@ -1,30 +1,109 @@
-import math
-import torch
-from torch import nn, Tensor
-import torch.nn.functional as F
-from constants import VOCAB
-from torch.utils.data.dataloader import default_collate
-from dataprep.descriptors import encoding_to_smiles
-import numpy as np
+
+from typing import Union
+import matplotlib.pyplot as plt
 from rdkit import Chem, RDLogger
 from rdkit.Chem.MolStandardize import rdMolStandardize
-import matplotlib.pyplot as plt
-from rdkit import Chem
 from rdkit.Chem.Draw import MolToImage
-from typing import Union
+from rdkit.DataStructs import TanimotoSimilarity
+from Levenshtein import distance as edit_distance
+from dataprep.descriptors import mols_to_ecfp
+from constants import VOCAB
 
 
-def draw_mol(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    im = MolToImage(mol)
-    plt.imshow(im)
-    plt.axis('off')
+def uniqueness(designs: list[str]) -> float:
+    """  Calculates the ratio of unique designs
+
+    :param designs: list of SMILES strings
+    :return: ratio of uniques
+    """
+    return len(set(designs))/len(designs)
+
+
+def novelty(designs: list[str], train_set: list[str]) -> float:
+    """ Calculates the fraction of unique designs that do not occur in the train set
+
+    :param designs: list of SMILES strings
+    :param train_set: train SMILES strings
+    :return: ratio of new designs
+    """
+    novel_designs = [smi for smi in set(designs) if smi not in train_set]
+
+    return len(novel_designs) / len(designs)
+
+
+def reconstruction(predicted_smiles: str, target_smiles: str) -> int:
+    """ checks if a SMILES string has been successfully reconstructed
+
+    :param predicted_smiles: generated SMILES string
+    :param target_smiles: target SMILES string
+    :return: 1 if reconstructed else 0
+    """
+
+    return 1 if predicted_smiles == target_smiles else 0
+
+
+def reconstruction_edit_distance(predicted_smiles: str, target_smiles: str, normalize: bool = True) -> float:
+    """ calculates the edit/Levenshtein distances between two strings
+
+    :param predicted_smiles: generated SMILES string
+    :param target_smiles: target SMILES string
+    :param normalize: normalizes the edit distance by the target string length (default=True)
+    :return: edit distance
+    """
+
+    dist = edit_distance(predicted_smiles, target_smiles)
+
+    if normalize:
+        dist = dist/len(target_smiles)
+
+    return dist
+
+
+def reconstruction_tanimoto_similarity(predicted_smiles: str, target_smiles: str, **kwargs) -> float:
+    """ calculates the Tanimoto similarity between two SMILES strings using ECFPs (2048 bits, radius 2 by default).
+    SMILES strings must be valid molecules!
+
+    :param predicted_smiles: generated SMILES string
+    :param target_smiles: target SMILES string
+    :param **kwargs: kwargs passed to mols_to_ecfp (e.g., radius=2, nbits=2048)
+    :return: Tanimoto similarity
+    """
+    pred_mol = Chem.MolFromSmiles(predicted_smiles)
+    target_mol = Chem.MolFromSmiles(target_smiles)
+
+    fps = mols_to_ecfp([pred_mol, target_mol], **kwargs)
+
+    return TanimotoSimilarity(fps[0], fps[1])
+
+
+def draw_mol_comparison(smiles1: str, smiles2: str = None):
+    """ Plots one or two molecules from their SMILES string """
+
+    im1 = MolToImage(Chem.MolFromSmiles(smiles1))
+    if smiles2 is None:
+        f, axarr = plt.subplots(1, 1, figsize=(3, 3))
+        axarr.imshow(im1)
+        axarr.axis('off')
+    else:
+        im2 = MolToImage(Chem.MolFromSmiles(smiles2))
+        f, axarr = plt.subplots(2, 1, figsize=(3, 6))
+        axarr[0].imshow(im1)
+        axarr[0].axis('off')
+        axarr[1].imshow(im2)
+        axarr[1].axis('off')
+
     plt.show()
 
 
-def smiles_validity(smiles: list[str]):
-    valid_smiles = get_valid_designs(smiles)
-    validity = len(valid_smiles) / len(smiles)
+def smiles_validity(smiles: list[str], return_invalids: bool = False) -> (float, list):
+    """ Checks SMILES validity over a list of SMILES strings
+
+    :param smiles: list of plausible SMILES strings
+    :param return_invalids: if True, returns None for invalids, if False (default), only returns valids
+    :return: ratio of valid molecules, list of SMILES
+    """
+    valid_smiles = get_valid_designs(smiles, return_invalids)
+    validity = len([smi for smi in valid_smiles if smi is not None]) / len(smiles)
 
     return validity, valid_smiles
 
@@ -63,7 +142,7 @@ def clean_design(smi: str) -> Union[str, None]:
     return can_smiles
 
 
-def get_valid_designs(design_list: list[str]) -> list[str]:
+def get_valid_designs(design_list: list[str], return_invalids: bool = False) -> list[str]:
     """
     Filters a list of SMILES strings to only keep the valid ones.
     Applies the `clean_design` function to each SMILES string in the list.
@@ -80,6 +159,8 @@ def get_valid_designs(design_list: list[str]) -> list[str]:
     RDLogger.DisableLog('rdApp.*')
     cleaned_designs = [clean_design(design) for design in design_list]
     RDLogger.EnableLog('rdApp.*')
+    if return_invalids:
+        return cleaned_designs
 
     return [design for design in cleaned_designs if design is not None]
 
@@ -89,18 +170,3 @@ def strip_smiles(smiles: list[str]):
     stripped = [smi.replace(start_token, '').replace(end_token, '').replace(padding_token, '') for smi in smiles]
 
     return stripped
-
-
-
-
-
-
-
-
-
-
-designs = ['COCCN(c1ccccc1)C(C)c1nn(C)cc1O', 'CC1CN(Cc2nnsc2Cl)CC1Nc1nccs1', 'N#Cc1cccc(/C=C/c2ccccc2)c1', 'CCOC1CN(CC2CC2)C2CCCOC12', 'O=C(NCc1ccc(Cl)cc1)c1ccncc1', 'CC1OC(O)C(S)C(O)C1O', 'COc1ccc2oc(=O)c(=O)[nH]c2c1', 'Cc1nc(CNc2cccc(Cl)c2Cl)cs1', 'CCn1c(=O)[nH]c2cc(C)nn2c1=O', 'CC(=CCC(N)Cc1ccccc1)c1nc(C)no1', 'Cc1cc(NCCNN2CCOCC2)n2nccc2n1', 'CN(C)C(=O)c1ccc(OC2CCOCC2)cc1', 'O=C1NC(CO)C(O)C(O)C1O', 'C/C(=N/NC(=S)N1CCNCC1)c1ccccc1', 'O=C(NCCN1CCOCC1)c1cc(Cl)ccc1Cl', 'Nc1cccc(Cl)c1CC=NNc1ccccn1', 'O=C(CCC1CCCCC1)NCCC1CCOCC1', 'NN(CC(=O)O)C(=O)NCCc1ccco1', 'CN1CCOc2nncc(N)c2C1=O', 'COC(=O)NC1=NC(c2ccccc2N)NN1', 'Oc1nc(CSc2nn[nH]n2)nc2ccccc12', 'O=C1NC(=S)NC1=Cc1cccs1', 'COc1ccc(/N=C/c2ccc(O)cc2)cc1', 'Cc1ccc(C(=O)NCc2cccnc2)o1']
-
-
-
-
