f9c42b7c5d19304ec1202e9cb8098b49e14ffa0a
derekvantilborg <dwvantilborg@gmail.com>
2024-04-22 13:14:37 +0200

    moved config to seperate yml files now

--- constants.py
+++ constants.py
@@ -5,22 +5,12 @@ ROOTDIR = "/Users/derekvantilborg/Dropbox/PycharmProjects/JointChemicalModel"
 
 
 DEFAULT_CONFIG = {'device': 'cuda' if torch.cuda.is_available() else 'cpu',
-                  'batch_size': 128,
+                  'batch_size': 256,
                   'num_workers': 1,
                   'grad_norm_clip': 1,
-                  'batch_end_callback_every': 1000,
-                  'val_molecules_to_sample': 1000,
+                  'batch_end_callback_every': 500,
+                  'val_molecules_to_sample': 500,
                   'out_path': os.path.join(ROOTDIR, 'results/chembl_vae')}
-
-
-VAE_PRETRAIN_HYPERPARAMETERS = {'input_dim': 2048,
-                                "latent_dim": 64,
-                                'out_dim': 2048,
-                                'beta': 0.001,
-                                'class_scaling_factor': 40,
-                                'variational_scale': 0.1,
-                                'lr': 3e-4}
-
 
 VOCAB = {'start_char': ':', 'end_char': ';', 'pad_char': '_', 'max_len': 62, 'vocab_size': 35,
          'start_idx': 0, 'end_idx': 33, 'pad_idx': 34,
