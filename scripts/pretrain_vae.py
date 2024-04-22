fb84274a8c5efa05dc89fd17bebe4efbc507add4
derekvantilborg <dwvantilborg@gmail.com>
2024-04-22 13:15:30 +0200

    cleaned loop

--- scripts/pretrain_vae.py
+++ scripts/pretrain_vae.py
@@ -16,15 +16,18 @@ OUT_DIR = os.path.join(ROOTDIR, 'results/chembl_vae_scaffs_trimmed')
 
 if __name__ == '__main__':
 
-    train_smiles = pd.read_csv('data/ChEMBL/train_smiles.csv').smiles.tolist()
-    train_dataset = MoleculeDataset(train_smiles, descriptor='ecfp')
+    train_smiles = pd.read_csv('data/ChEMBL/chembl_train_smiles.csv').smiles.tolist()
+    train_dataset = MoleculeDataset(train_smiles, descriptor='smiles')
 
-    val_smiles = pd.read_csv('data/ChEMBL/val_smiles.csv').smiles.tolist()
-    val_dataset = MoleculeDataset(val_smiles, descriptor='ecfp')
+    val_smiles = pd.read_csv('data/ChEMBL/chembl_val_smiles.csv').smiles.tolist()
+    val_dataset = MoleculeDataset(val_smiles, descriptor='smiles')
 
-    VAE_PRETRAIN_HYPERPARAMETERS['latent_dim'] = 64
+    # val_dataset[0][0].shape >>> 167
+    VAE_PRETRAIN_HYPERPARAMETERS['latent_dim'] = 128
+    VAE_PRETRAIN_HYPERPARAMETERS['lr'] = 3e-4
+    VAE_PRETRAIN_HYPERPARAMETERS['hidden_size'] = 512
 
-    config = Config(max_iters=100000, batch_size=256, batch_end_callback_every=1000, out_path=OUT_DIR)
+    config = Config(max_iters=1000, batch_size=256, batch_end_callback_every=10, out_path=OUT_DIR)
     config.set_hyperparameters(**VAE_PRETRAIN_HYPERPARAMETERS)
 
     model, trainer = train_vae(config, train_dataset, val_dataset)
