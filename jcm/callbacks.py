ecd7d461616723d6ad63d933af3315589a514e01
derekvantilborg <dwvantilborg@gmail.com>
2024-06-13 11:58:36 +0200

    added jvae callback

--- jcm/callbacks.py
+++ jcm/callbacks.py
@@ -110,12 +110,56 @@ def mlp_callback(trainer):
 
         # Update the training history and save if a path is given in the config
         trainer.append_history(iter_num=trainer.iter_num, train_loss=train_loss, val_loss=val_loss,
-                               balanced_accuracy=b_acc, mean_sample_entropy=entropy)
+                               balanced_accuracy=b_acc)
+        if trainer.config.out_path is not None:
+            trainer.get_history(os.path.join(config.out_path, f"training_history.csv"))
+
+        print(f"Iter: {i}, train loss: {train_loss:.4f}, val loss: {val_loss:.4f}, balanced accuracy: {b_acc:.4f}")
+
+
+def jvae_callback(trainer):
+    config = trainer.config
+    i = trainer.iter_num
+
+    # Check if we want to perform a callback
+    if should_perform_callback(config.batch_end_callback_every, i):
+
+        # Save model checkpoint
+        if config.out_path is not None:
+            trainer.model.save_weights(os.path.join(config.out_path, f"vae_{trainer.iter_num}.pt"))
+
+        # Predict from the validation set
+        pred = trainer.model.predict(trainer.val_dataset, sample=True)
+        token_probs_N_S_C, y_logprobs_N_K_C, molecule_reconstruction_losses, val_loss, target_ys, target_smiles = pred
+
+        # Get the losses
+        val_loss = val_loss.item()
+        train_loss = trainer.loss.item()
+
+        # Balanced accuracy
+        preds, uncertainty = logits_to_pred(y_logprobs_N_K_C, return_binary=True, return_uncertainty=True)
+        b_acc = balanced_accuracy_score(preds, target_ys)
+
+        # reconstruction
+        designs = probs_to_smiles(token_probs_N_S_C)
+
+        # Clean designs
+        designs_clean = strip_smiles(designs)
+        validity, valid_smiles = smiles_validity(designs_clean, return_invalids=True)
+
+        # levensthein distance. This is calculated between the stripped SMILES strings. This means that if the model
+        # does not learn how to place the end token, this metric is off.
+        edist = np.mean([reconstruction_edit_distance(i, j) for i, j in zip(designs_clean, target_smiles)])
+
+        # Update the training history and save if a path is given in the config
+        trainer.append_history(iter_num=trainer.iter_num, train_loss=train_loss, val_loss=val_loss,
+                               balanced_accuracy=b_acc, validity=validity, edit_distance=edist)
         if trainer.config.out_path is not None:
             trainer.get_history(os.path.join(config.out_path, f"training_history.csv"))
 
         print(f"Iter: {i}, train loss: {train_loss:.4f}, val loss: {val_loss:.4f}, balanced accuracy: {b_acc:.4f}, "
-              f"mean sample entropy: {entropy:.4f}")
+              f"validity: {validity:.4f}, edit dist: {edist:.4f}, example: {designs[0]}, target: {target_smiles[0]}")
+
 
 
 # @torch.no_grad()
