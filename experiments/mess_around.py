53bc3a58862a37ac6ab2ad6d0fbeaf9b7f5cda38
derekvantilborg <dwvantilborg@gmail.com>
2024-06-28 11:16:36 +0200

    big cleanup of files

--- experiments/mess_around.py
+++ experiments/mess_around.py
@@ -12,40 +12,41 @@ from sklearn.model_selection import ParameterGrid
 from jcm.datasets import MoleculeDataset
 from jcm.training import Trainer
 from jcm.config import Config, load_settings
-from cheminformatics.descriptors import one_hot_encode
+
 
 
 PATH_TRAIN_SMILES = ospj('data', 'ChEMBL', 'chembl_train_smiles.csv')
 PATH_VAL_SMILES = ospj('data', 'ChEMBL', 'chembl_val_smiles.csv')
-DEFAULT_SETTINGS_PATH = "experiments/hyperparams/vae_pretrain_default.yml"
+# DEFAULT_SETTINGS_PATH = "experiments/hyperparams/vae_pretrain_default.yml"
+DEFAULT_SETTINGS_PATH = "experiments/hyperparams/autoregressive_lstm_default.yml"
+
 
 experiment_settings = load_settings(DEFAULT_SETTINGS_PATH)
 config = experiment_settings['training_config']
-config['batch_end_callback_every'] = 10
+config['batch_end_callback_every'] = 1000
 hypers = experiment_settings['hyperparameters']
 
-train_smiles = pd.read_csv(PATH_TRAIN_SMILES).smiles.tolist()[:10000]
-train_dataset = MoleculeDataset(train_smiles, descriptor=config['descriptor'], randomize_smiles=True)
+train_smiles = pd.read_csv(PATH_TRAIN_SMILES).smiles.tolist()[:1000000]
+train_dataset = MoleculeDataset(train_smiles, descriptor=config['descriptor'], randomize_smiles=False)
 
-val_smiles = pd.read_csv(PATH_VAL_SMILES).smiles.tolist()[:1000]
-val_dataset = MoleculeDataset(val_smiles, descriptor=config['descriptor'], randomize_smiles=True)
+val_smiles = pd.read_csv(PATH_VAL_SMILES).smiles.tolist()[:10000]
+val_dataset = MoleculeDataset(val_smiles, descriptor=config['descriptor'], randomize_smiles=False)
 
 config_ = Config(**config)
 config_.set_hyperparameters(**hypers)
 
 
-from jcm.models import DeNovoLSTM
-from jcm.callbacks import denovo_lstm_callback
+from jcm.models import SmilesMLP, ECFPMLP, JointChemicalModel, VAE, DeNovoLSTM
+from jcm.callbacks import mlp_callback, jvae_callback, vae_callback, denovo_lstm_callback
+
 
 model = DeNovoLSTM(config_)
+
 
 T = Trainer(config_, model, train_dataset, val_dataset)
 if val_dataset is not None:
     T.set_callback('on_batch_end', denovo_lstm_callback)
 T.run()
-
-
-
 
 
 
