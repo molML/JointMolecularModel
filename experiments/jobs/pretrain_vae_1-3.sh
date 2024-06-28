f0ecbd3002975741bdfd1cc6309736f80520dc89
derekvantilborg <dwvantilborg@gmail.com>
2024-06-28 17:02:56 +0200

    trying to get this working

--- experiments/jobs/pretrain_vae_1-3.sh
+++ experiments/jobs/pretrain_vae_1-3.sh
@@ -9,15 +9,13 @@
 
 experiment_name="vae_pretraining"
 
-echo 'experiment_name = $experiment_name'
 
 project_path="$HOME/projects/JointChemicalModel"
 experiment_script_path="$project_path/experiments/3.0_vae_pretraining.py"
 
 out_path="$project_path/results/$experiment_name"
-log_path="$HOME/results/logs"
+log_path="project_path/results/logs"
 
-echo 'past the vars'
 
 
 source $HOME/anaconda3/etc/profile.d/conda.sh
@@ -28,17 +26,14 @@ export PYTHONPATH="$PYTHONPATH:$project_path"
 $HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 1 > "$log_path/${experiment_name}_1.log" &
 pid1=$!
 
-echo 'past job 1'
 
 $HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 2 > "$log_path/${experiment_name}_2.log" &
 pid2=$!
 
-echo 'past job 2'
 
 $HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 3 > "$log_path/${experiment_name}_3.log" &
 pid3=$!
 
-echo 'past job 3'
 
 wait $pid1
 wait $pid2
