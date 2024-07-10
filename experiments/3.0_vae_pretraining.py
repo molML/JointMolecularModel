a0efe169338073419dbdaced86a219374c9ea6ee
derekvantilborg <dwvantilborg@gmail.com>
2024-07-10 15:02:13 +0200

    write batch experiment scripts automatically

--- experiments/3.0_vae_pretraining.py
+++ experiments/3.0_vae_pretraining.py
@@ -8,6 +8,7 @@ June 2024
 
 import os
 from os.path import join as ospj
+from itertools import batched
 import pandas as pd
 from sklearn.model_selection import ParameterGrid
 from jcm.callbacks import vae_callback
@@ -72,8 +73,51 @@ def train_model(config):
     T.run()
 
 
-def write_job_script(hyperparameters):
-    pass
+def write_job_script(experiments: list[int], experiment_name: str = "vae_pretraining",
+                     experiment_script: str = "3.0_vae_pretraining.py", partition: str = 'gpu', ntasks: str = '18',
+                     gpus_per_node: str = 1, time: str = "4:00:00") -> None:
+    """
+    :param experiments: list of experiment numbers, e.g. [0, 1, 2]
+    """
+
+    jobname = experiment_name + '_' + '_'.join([str(i) for i in experiments])
+
+    lines = []
+    lines.append('# !/bin/bash\n')
+    lines.append(f'# SBATCH --job-name={jobname}\n')
+    lines.append(f'# SBATCH --output=/home/tilborgd/projects/JointChemicalModel/results/out/{jobname}.out\n')
+    lines.append(f'# SBATCH -p {partition}\n')
+    lines.append('# SBATCH -N 1\n')
+    lines.append(f'# SBATCH --ntasks={ntasks}\n')
+    lines.append(f'# SBATCH --gpus-per-node={gpus_per_node}\n')
+    lines.append(f'# SBATCH --time={time}\n')
+    lines.append('\n')
+    lines.append(f'experiment_name="{experiment_name}"\n')
+    lines.append('\n')
+    lines.append('project_path="$HOME/projects/JointChemicalModel"\n')
+    lines.append(f'experiment_script_path="$project_path/experiments/{experiment_script}"\n')
+    lines.append('\n')
+    lines.append('out_path="$project_path/results/$experiment_name"\n')
+    lines.append('log_path="$project_path/results/logs"\n')
+    lines.append('\n')
+    lines.append('source $HOME/anaconda3/etc/profile.d/conda.sh\n')
+    lines.append('export PYTHONPATH="$PYTHONPATH:$project_path"\n')
+
+    for i, exp in enumerate(experiments):
+        lines.append('\n')
+        lines.append('$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment EX > "$log_path/${experiment_name}_EX.log" &\n'.replace('EX', str(exp)))
+        lines.append(f'pid{i+1}=$!\n')
+
+    lines.append('\n')
+    for i, exp in enumerate(experiments):
+        lines.append(f'wait $pid{i+1}\n')
+    lines.append('\n')
+
+    outpath = ospj(ROOTDIR, 'experiments', 'jobs', jobname + '.sh')
+
+    # Write the modified lines back to the file
+    with open(outpath, 'w') as file:
+        file.writelines(lines)
 
 
 if __name__ == '__main__':
@@ -100,12 +144,27 @@ if __name__ == '__main__':
                     'beta': [0.001, 0.0001],
                    }
 
+    hyper_grid = ParameterGrid(SEARCH_SPACE)
+
     out_path = args.o
     experiment = int(args.experiment)
 
-    experiment_hypers = ParameterGrid(SEARCH_SPACE)[experiment]
+    experiment_hypers = hyper_grid[experiment]
     experiment_settings = {'out_path': out_path, 'experiment_name': str(experiment)}
 
     config = configure_config(hypers=experiment_hypers, settings=experiment_settings)
 
     train_model(config)
+
+    #
+    # experiment_batches = [i for i in batched(range(len(hyper_grid)), 5)]
+    # for batch in experiment_batches:
+    #     write_job_script(experiments=batch,
+    #                      experiment_name="vae_pretraining",
+    #                      experiment_script="3.0_vae_pretraining.py",
+    #                      partition='gpu',
+    #                      ntasks='18',
+    #                      gpus_per_node=1,
+    #                      time="120:00:00"
+    #                      )
+    #
