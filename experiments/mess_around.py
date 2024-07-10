5c2fed7f5613d8250079cdc9ceeefd14e609b7f3
derekvantilborg <dwvantilborg@gmail.com>
2024-07-10 15:02:44 +0200

    .

--- experiments/mess_around.py
+++ experiments/mess_around.py
@@ -15,21 +15,22 @@ from jcm.config import Config, load_settings
 
 
 
-PATH_TRAIN_SMILES = ospj('data', 'ChEMBL', 'chembl_train_smiles.csv')
-PATH_VAL_SMILES = ospj('data', 'ChEMBL', 'chembl_val_smiles.csv')
+data_path = ospj('data/split/ChEMBL_33_split.csv')
+chembl = pd.read_csv(data_path)
+
 # DEFAULT_SETTINGS_PATH = "experiments/hyperparams/vae_pretrain_default.yml"
 DEFAULT_SETTINGS_PATH = "experiments/hyperparams/autoregressive_lstm_default.yml"
 
 
 experiment_settings = load_settings(DEFAULT_SETTINGS_PATH)
 config = experiment_settings['training_config']
-config['batch_end_callback_every'] = 1000
+config['batch_end_callback_every'] = 10
 hypers = experiment_settings['hyperparameters']
 
-train_smiles = pd.read_csv(PATH_TRAIN_SMILES).smiles.tolist()[:1000000]
+train_smiles = chembl[chembl['split'] == 'train'].smiles.tolist()[:1000000]
 train_dataset = MoleculeDataset(train_smiles, descriptor=config['descriptor'], randomize_smiles=False)
 
-val_smiles = pd.read_csv(PATH_VAL_SMILES).smiles.tolist()[:10000]
+val_smiles = chembl[chembl['split'] == 'val'].smiles.tolist()[:10000]
 val_dataset = MoleculeDataset(val_smiles, descriptor=config['descriptor'], randomize_smiles=False)
 
 config_ = Config(**config)
@@ -47,8 +48,4 @@ T = Trainer(config_, model, train_dataset, val_dataset)
 if val_dataset is not None:
     T.set_callback('on_batch_end', denovo_lstm_callback)
 T.run()
-
-
-
-
 
