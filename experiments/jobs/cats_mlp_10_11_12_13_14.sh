d4e5185177246dac9b2e96788f61fb6ba1ffed2a
derekvantilborg <dwvantilborg@gmail.com>
2024-07-26 13:07:57 +0200

    ecfp/cats mlp jobscripts

---
+++ experiments/jobs/cats_mlp_10_11_12_13_14.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=cats_mlp_10_11_12_13_14
+#SBATCH --output=/home/tilborgd/projects/JointChemicalModel/results/out/cats_mlp_10_11_12_13_14.out
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --ntasks=18
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:00
+
+project_path="$HOME/projects/JointChemicalModel"
+experiment_script_path="$project_path/experiments/4.2_cats_mlp.py"
+
+out_path="$project_path/results"
+log_path="$project_path/results/logs"
+
+source $HOME/anaconda3/etc/profile.d/conda.sh
+export PYTHONPATH="$PYTHONPATH:$project_path"
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 10 > "$log_path/${experiment_name}_10.log" &
+pid1=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 11 > "$log_path/${experiment_name}_11.log" &
+pid2=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 12 > "$log_path/${experiment_name}_12.log" &
+pid3=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 13 > "$log_path/${experiment_name}_13.log" &
+pid4=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 14 > "$log_path/${experiment_name}_14.log" &
+pid5=$!
+
+wait $pid1
+wait $pid2
+wait $pid3
+wait $pid4
+wait $pid5
+
---
+++ experiments/jobs/ecfp_mlp_15_16_17_18_19.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=ecfp_mlp_15_16_17_18_19
+#SBATCH --output=/home/tilborgd/projects/JointChemicalModel/results/out/ecfp_mlp_15_16_17_18_19.out
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --ntasks=18
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:00
+
+project_path="$HOME/projects/JointChemicalModel"
+experiment_script_path="$project_path/experiments/4.2_ecfp_mlp.py"
+
+out_path="$project_path/results"
+log_path="$project_path/results/logs"
+
+source $HOME/anaconda3/etc/profile.d/conda.sh
+export PYTHONPATH="$PYTHONPATH:$project_path"
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 15 > "$log_path/${experiment_name}_15.log" &
+pid1=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 16 > "$log_path/${experiment_name}_16.log" &
+pid2=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 17 > "$log_path/${experiment_name}_17.log" &
+pid3=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 18 > "$log_path/${experiment_name}_18.log" &
+pid4=$!
+
+$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment 19 > "$log_path/${experiment_name}_19.log" &
+pid5=$!
+
+wait $pid1
+wait $pid2
+wait $pid3
+wait $pid4
+wait $pid5
+
