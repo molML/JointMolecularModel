8c0a6b877de1b248539bae6df87e09542aa16e9d
derekvantilborg <dwvantilborg@gmail.com>
2024-07-30 19:24:09 +0200

    works now

--- experiments/4.4_smiles_mlp.py
+++ experiments/4.4_smiles_mlp.py
@@ -17,9 +17,9 @@ from jcm.models import SmilesMLP
 from jcm.callbacks import mlp_callback
 
 
-def write_job_script(experiments: list[int], experiment_name: str = "smiles_mlp",
-                     experiment_script: str = "4.4_smiles_mlp.py", partition: str = 'gpu', ntasks: str = '18',
-                     gpus_per_node: str = 1, time: str = "120:00:00") -> None:
+def write_job_script(experiments: list[int], out_paths: list[str] = 'results', experiment_name: str = "cats_mlp",
+                     experiment_script: str = "4.2_cats_mlp.py", partition: str = 'gpu', ntasks: str = '18',
+                     gpus_per_node: str = 1, time: str = "4:00:00") -> None:
     """
     :param experiments: list of experiment numbers, e.g. [0, 1, 2]
     """
@@ -39,15 +39,14 @@ def write_job_script(experiments: list[int], experiment_name: str = "smiles_mlp"
     lines.append('project_path="$HOME/projects/JointChemicalModel"\n')
     lines.append(f'experiment_script_path="$project_path/experiments/{experiment_script}"\n')
     lines.append('\n')
-    lines.append('out_path="$project_path/results"\n')
     lines.append('log_path="$project_path/results/logs"\n')
     lines.append('\n')
     lines.append('source $HOME/anaconda3/etc/profile.d/conda.sh\n')
     lines.append('export PYTHONPATH="$PYTHONPATH:$project_path"\n')
 
-    for i, exp in enumerate(experiments):
+    for i, (exp, out_path) in enumerate(zip(experiments, out_paths)):
         lines.append('\n')
-        lines.append('$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o $out_path -experiment EX > "$log_path/${experiment_name}_EX.log" &\n'.replace('EX', str(exp)))
+        lines.append('$HOME/anaconda3/envs/karman/bin/python -u $experiment_script_path -o OUT_PATH -experiment EX > "$log_path/XE.log" &\n'.replace('EX', str(exp)).replace('XE', f"{experiment_name}_{exp}").replace('OUT_PATH', out_path))
         lines.append(f'pid{i+1}=$!\n')
 
     lines.append('\n')
@@ -55,10 +54,14 @@ def write_job_script(experiments: list[int], experiment_name: str = "smiles_mlp"
         lines.append(f'wait $pid{i+1}\n')
     lines.append('\n')
 
-    outpath = ospj(ROOTDIR, 'experiments', 'jobs', jobname + '.sh')
+    # Move all output files to the project directory
+    for i, out_path in enumerate(out_paths):
+        # lines.append(f'mkdir -p $HOME/../../projects/prjs1021/JointChemicalModel/{os.path.dirname(out_path)}\n')
+        lines.append(f'mv $project_path/{out_path} /projects/prjs1021/JointChemicalModel/{os.path.dirname(out_path)}\n\n')
+    lines.append('\n')
 
     # Write the modified lines back to the file
-    with open(outpath, 'w') as file:
+    with open(ospj(ROOTDIR, 'experiments', 'jobs', jobname + '.sh'), 'w') as file:
         file.writelines(lines)
 
 
@@ -66,6 +69,7 @@ if __name__ == '__main__':
 
     MODEL = SmilesMLP
     CALLBACK = mlp_callback
+    EXPERIMENT_NAME = "smiles_mlp"
     DEFAULT_SETTINGS_PATH = "experiments/hyperparams/smiles_mlp_default.yml"
     HYPERPARAM_GRID = {'mlp_hidden_dim': [1024, 2048],
                        'mlp_n_layers': [2, 3, 4, 5],
@@ -86,8 +90,11 @@ if __name__ == '__main__':
 
     # experiment_batches = [i for i in batched(range(len(all_datasets)), 5)]
     # for batch in experiment_batches:
+    #     out_paths = [f"results/{EXPERIMENT_NAME}/{all_datasets[exp_i]}" for exp_i in batch]
+    #
     #     write_job_script(experiments=batch,
-    #                      experiment_name="smiles_mlp",
+    #                      out_paths=out_paths,
+    #                      experiment_name=EXPERIMENT_NAME,
     #                      experiment_script="4.4_smiles_mlp.py",
     #                      partition='gpu',
     #                      ntasks='18',
@@ -105,16 +112,12 @@ if __name__ == '__main__':
     experiment = int(args.experiment)
     dataset_name = all_datasets[experiment]
 
-    out_path = 'results'
-    experiment = 0
-    dataset_name = all_datasets[experiment]
-
     # perform the experiment ###########################################################################################
 
     best_hypers = mlp_hyperparam_tuning(MODEL, CALLBACK, dataset_name, DEFAULT_SETTINGS_PATH, HYPERPARAM_GRID)
 
     settings = load_settings(DEFAULT_SETTINGS_PATH)
-    config_dict = settings['training_config'] | {'dataset_name': dataset_name}
+    config_dict = settings['training_config'] | {'dataset_name': dataset_name, 'out_path': out_path}
     hyperparameters = settings['hyperparameters'] | best_hypers
 
     config = Config(**config_dict)
@@ -124,7 +127,7 @@ if __name__ == '__main__':
     prep_outdir(config)
 
     # save best hypers
-    save_settings(config, ospj(config.out_path, config.experiment_name, config.dataset_name, 'experiment_settings.yml'))
+    save_settings(config, ospj(config.out_path, 'experiment_settings.yml'))
 
     # perform model training with cross validation and save results
     results = nn_cross_validate(MODEL, CALLBACK, config)
