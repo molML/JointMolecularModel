a6e8fa24cd239ee61c9257b2bd4a881251e1b6bb
derekvantilborg <dwvantilborg@gmail.com>
2024-08-02 14:39:13 +0200

    outfile copy-pasting

--- experiments/4.3_cats_mlp.py
+++ experiments/4.3_cats_mlp.py
@@ -55,9 +55,11 @@ def write_job_script(experiments: list[int], out_paths: list[str] = 'results', e
 
     # Move all output files to the project directory
     for i, out_path in enumerate(out_paths):
-        # lines.append(f'mkdir -p $HOME/../../projects/prjs1021/JointChemicalModel/{os.path.dirname(out_path)}\n')
-        lines.append(f'mv $project_path/{out_path} /projects/prjs1021/JointChemicalModel/{os.path.dirname(out_path)}\n\n')
-    lines.append('\n')
+        source = f"$project_path/{out_path}"
+        destination = f"/projects/prjs1021/JointChemicalModel/{out_path}"
+
+        lines.append(f'cp -r {source} {destination}\n')
+        lines.append(f"if [ $? -eq 0 ]; then\n    rm -rf {source}\nfi\n\n")
 
     # Write the modified lines back to the file
     with open(ospj(ROOTDIR, 'experiments', 'jobs', jobname + '.sh'), 'w') as file:
