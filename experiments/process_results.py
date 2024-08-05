4893f2f7082f3f153298d9b7092202c2e03a5220
derekvantilborg <dwvantilborg@gmail.com>
2024-08-05 11:12:12 +0200

    getting local results

--- experiments/process_results.py
+++ experiments/process_results.py
@@ -1,35 +1,114 @@
+
 import os
 from os.path import join as ospj
 import pandas as pd
 import numpy
 from constants import ROOTDIR
 
+from cheminformatics.descriptors import mols_to_ecfp
+from cheminformatics.utils import smiles_to_mols
+from rdkit.DataStructs import BulkTanimotoSimilarity
+from cheminformatics.utils import get_scaffold
+from rdkit import Chem
+from cheminformatics.eval import tani_sim_to_train, substructure_sim_to_train
+import numpy as np
+import shutil
+
+
+def get_local_results() -> None:
+    """ Get the results from the experiments that were run locally and move them to 'all_results """
+
+    experiments = ['cats_random_forest', 'ecfp_random_forest']
+    out_path = ospj(RESULTS, 'all_results')
+
+    for experi in experiments:
+        datasets = [i for i in os.listdir(ospj(RESULTS, experi)) if not i.startswith('.')]
+        for dataset in datasets:
+            src = ospj(RESULTS, experi, dataset, 'results_preds.csv')
+            dst = ospj(out_path, f"{experi}_{dataset}_results_preds.csv")
+            shutil.copyfile(src, dst)
+
+
+def combine_results() -> pd.DataFrame:
+    pass
+
+
+def compute_distances(df) -> pd.DataFrame:
+    pass
+
+
 if __name__ == '__main__':
 
     RESULTS = 'results'
 
-    # move to root dir
+    # Move to root dir
     os.chdir(ROOTDIR)
 
-    # RF control expriments
-    datasets = [i for i in os.listdir(ospj(RESULTS, 'ecfp_random_forest')) if not i.startswith('.')]
+    # Get the results from the experiments that were run locally (ecfp/cats + random forest)
+    get_local_results()
 
-    results = []
-    for dataset in datasets:
-        df = pd.read_csv(ospj(RESULTS, 'ecfp_random_forest', dataset, 'results_metrics.csv'))
-        df['dataset'] = dataset
-        df['descriptor'] = 'ecfp'
-        results.append(df)
+    # Put all results in one big file
+    df = combine_results()
 
-        df = pd.read_csv(ospj(RESULTS, 'cats_random_forest', dataset, 'results_metrics.csv'))
-        df['dataset'] = dataset
-        df['descriptor'] = 'cats'
-        results.append(df)
-
-
-    results = pd.concat(results)
-
-    # Group by 'GroupID' and calculate mean and standard deviation
-    results = results.groupby(['dataset', 'descriptor']).agg(['mean', 'std'])
-
-    results.to_csv(ospj(RESULTS, 'processed', 'random_forest.csv'))
+    # Compute distance metrics
+    df = compute_distances(df)
+        # 1. Tanimoto
+        # 2. MCS
+    #
+    #
+    # data_path = "results/ecfp_random_forest/CHEMBL4792_Ki/results_preds.csv"
+    # df = pd.read_csv(data_path)
+    #
+    # df.columns
+    #
+    # train_smiles = list(set(df[df['split'] == 'train'].smiles.tolist()))
+    # smiles = list(set(df[df['split'] == 'ood'].smiles.tolist()))
+    #
+    # Tfull = tani_sim_to_train(smiles, train_smiles, scaffold=False)
+    # Tscaf = tani_sim_to_train(smiles, train_smiles, scaffold=True)
+    #
+    # Sfull = substructure_sim_to_train(smiles, train_smiles, scaffold=False)
+    # Sscaf = substructure_sim_to_train(smiles, train_smiles, scaffold=True)
+    #
+    # from scipy.stats import pearsonr
+    #
+    # pearsonr(Tfull, Sfull)
+    #
+    # paracetamol = "CC(=O)Nc1ccc(O)cc1"
+    # ibuprofen = "CC(C)Cc1ccc(cc1)[C@@H](C)C(=O)O"
+    #
+    # paracetamol = Chem.MolFromSmiles(paracetamol)
+    # ibuprofen = Chem.MolFromSmiles(ibuprofen)
+    #
+    # MCSS = MCSSimilarity()
+    # MCSS.calc_similarity(paracetamol, ibuprofen, symmetric=False)
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    # results = []
+    # for dataset in datasets:
+    #     df = pd.read_csv(ospj(RESULTS, 'ecfp_random_forest', dataset, 'results_metrics.csv'))
+    #     df['dataset'] = dataset
+    #     df['descriptor'] = 'ecfp'
+    #     results.append(df)
+    #
+    #     df = pd.read_csv(ospj(RESULTS, 'cats_random_forest', dataset, 'results_metrics.csv'))
+    #     df['dataset'] = dataset
+    #     df['descriptor'] = 'cats'
+    #     results.append(df)
+    #
+    #
+    # results = pd.concat(results)
+    #
+    # # Group by 'GroupID' and calculate mean and standard deviation
+    # results = results.groupby(['dataset', 'descriptor']).agg(['mean', 'std'])
+    #
+    # results.to_csv(ospj(RESULTS, 'processed', 'random_forest.csv'))
+    #
+    #
+    #
+    #
